23ec04f0871b7069bfae94058c45f52f494807c2
Håkon Ødegård Løvdal <me@hakloev.no>
2014-10-05 11:04:14 +0200

    Initated repo for project

---
+++ server.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect, url_for
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('layout.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', 
+            port=80, 
+            debug=True)
