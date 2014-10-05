e2d78468f1656369d2302542318ca25d8499a4f6
Håkon Ødegård Løvdal <me@hakloev.no>
2014-10-05 23:35:37 +0200

    most of the base shit, ready for ninja coding

--- server.py
+++ server.py
@@ -5,7 +5,31 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/attractions/<int:attractionId>")
+def attractions(attractionId):
+    print("DEBUG: Attraction requested: %d" % (attractionId))
+    if attractionId == 1:
+        return render_template("attractions/attraction1.html")
+    else:
+        return "Can't find the attractionId %d" % (attractionId)
+
+@app.route("/restaurant")
+def restaurant():
+    return "Here will be restaurant html"
+
+@app.route("/events")
+def events():    
+    return "Here will be events html"
+
+@app.route("/tickets")
+def tickets():
+    return "Here will be tickets html"
+
+@app.route("/contact")
+def contact():
+    return "Here will be contact html"
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', 
+    app.run(host='127.0.0.1', 
             port=80, 
             debug=True)
