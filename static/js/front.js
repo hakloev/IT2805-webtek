720fc0a78fbd9e7d7d0f9ab05265857fe4396741
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-11-06 15:53:09 +0100

    responsive, mobile first

--- static/js/front.js
+++ static/js/front.js
@@ -1,1 +1,29 @@
-console.log('front.js up-and-running');
+var responsive = (function () {
+    var mobile = document.createElement('div');
+    mobile.className = 'nav-mobile';
+    document.querySelector('.nav').appendChild(mobile);   
+
+    var hasClass = function(element, className) {
+        return new RegExp(' ' + className + ' ').test(' ' + element.className + ' ');
+    }
+
+    var toggleClass = function(element, className) {
+        var newClass = ' ' + element.className.replace(/[\t\r\n]/g, '') + ' ';
+        if (hasClass(element, className)) {
+            while (newClass.indexOf(' ' + className + ' ') >= 0) {
+                newClass = newClass.replace(' ' + className + ' ', ' ');
+            }
+            element.className = newClass.replace(/^\s+|\s+$/g, '');
+        } else {
+            element.className += ' ' + className;
+        }
+    }
+
+    var mobileNav = document.querySelector('.nav-mobile');
+    var toggle = document.querySelector('.nav-list');
+    mobileNav.addEventListener('click', function() {
+        toggleClass(this, 'nav-mobile-open');
+        toggleClass(toggle, 'nav-active');
+    });
+
+})();
