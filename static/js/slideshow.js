9944a28809afbc06a2e5e8295ed07e62befebe6c
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-11-17 11:11:02 +0100

    transitions and stuff

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -2,36 +2,74 @@
  * Created by trulsmp on 14/11/14.
  */
 var image1 = new Image();
-image1.src = "static/images/rides/sklia.JPG";
+image1.src = "/static/images/rides/sklia.JPG";
 var image2 = new Image();
 image2.src = "/static/images/rides/lightning.JPG";
 var image3 = new Image();
 image3.src = "/static/images/rides/ghostville.JPG";
 var image4 = new Image();
 image4.src = "/static/images/rides/svinghuska.JPG";
-var img = ["Sklia",
-"Lightning",
-"GhostVille",
-"Svinghuska",]
 
+var Slideshow = (function() {
+    
+    var url = "/static/images/rides/";
+    var step = 1;
+    var img = ["Sklia",
+               "Lightning",
+               "GhostVille",
+               "Svinghuska"];
+    var prevImg = img[0];
 
-var step=1;
-var slideshow = function () {
-    document.images.slide.src = eval("image"+step+".src");
-    var imageName = img[step-1];
-    document.getElementById("imagename").innerHTML = imageName;
-
-    if(step<4) {
-        step++;
+    var init = function() {
+        container = document.getElementById("slideshowcontainer");
+        for (var i = 0; i < img.length; i++) {
+            var image = document.createElement("img");
+            image.className = "slide";
+            image.id = img[i];
+            image.src = url + img[i] + ".JPG";
+            image.alt = "Attraksjon: " + img[i];
+            container.appendChild(image);
+            if (i == 0) {
+                image.className += " " + "showSlide";
+            }
+        }
+        slideshow();
     }
-    else{
-        step=1;
+    
+    var addClass = function(e, name) {
+        var c = e.className;
+        if (c) c += " " + name;
+        e.className = c;
     }
-    setTimeout("slideshow()",5000);
 
-};
+    var removeClass = function(e, name) {
+        var c = e.className; 
+        e.className = c.replace(name, "").replace(/ /g, " ").replace(/^ | $/g, ""); 
+    }
 
-slideshow();
+    var slideshow = function () {
+        removeClass(document.getElementById(prevImg), "showSlide");
+        var imageName = img[step-1];
+        var image = document.getElementById(imageName);
+        addClass(image, "showSlide");
+        document.getElementById("imagename").innerHTML = imageName;
+        prevImg = imageName;
+        
+        if(step<4) {
+            step++;
+        }
+        else{
+            step=1;
+        }
+        setTimeout(slideshow ,5000);
+    }
 
+    return {
+        init:init
+    }
 
+})();
 
+window.addEventListener('load', function() {
+    Slideshow.init();
+});
