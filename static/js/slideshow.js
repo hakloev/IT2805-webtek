d95e0ad5425a1dcd7f36988c3a322282a38c4d12
Håkon Ødegård Løvdal <hakloev@gmail.com>
2014-11-17 17:56:07 +0100

    slideshow

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -17,7 +17,7 @@ var Slideshow = (function() {
             var image = document.createElement("img");
             image.className = "slide";
             image.id = img[i];
-            image.src = (url + img[i] + ".JPG").toLowerCase();
+            image.src = ((url + img[i]).toLowerCase() + ".JPG");
             image.alt = "Attraksjon: " + img[i];
             container.appendChild(image);
             if (i == 0) {
