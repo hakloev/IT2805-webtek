9e7ccc8b35013ee368fd70debafe5a181e8fd4a4
Truls Mørk Pettersen <trulsmp@stud.ntnu.no>
2014-11-18 14:54:00 +0100

    added tickets xml

--- server.py
+++ server.py
@@ -3,6 +3,9 @@
 
 import os
 from flask import Flask, render_template, flash, redirect, url_for, request
+from xml.etree import ElementTree as ET
+from xml.dom import minidom
+
 
 app = Flask(__name__)
 app.secret_key = 'r\xe4@\x00\xa4\xf9\xe6.@S\x08/\xday\xf8\x9b\xfeR\xbe\x17\xfdK[\x90'
@@ -42,6 +45,8 @@ def events():
 def tickets():
     if request.method == "POST":
         flash('Takk for kjøpet! En bekreftelse på %s billett(er) er sendt til %s!' % (request.form['antallBilletter'], request.form['email']))
+        print(request.form)
+        saveTickets(request.form)
         return render_template('index.html')
     else:
         return render_template('subpages/tickets.html')
@@ -57,6 +62,42 @@ def contact():
 @app.route("/references")
 def references():
     return render_template('subpages/references.html')
+
+
+def saveTickets(form):
+
+    #Appends the ticket information to a xml file to be showed on the website
+    xml = ET.parse('static/tickets/tickets.xml')
+    ticks = xml.getroot()
+    ticket = ET.Element('ticket')
+    name = ET.Element('name')
+    name.text = form['name']
+    email = ET.Element('email')
+    email.text = form['email']
+    antallBilletter = ET.Element('antallBilletter')
+    antallBilletter.text = form['antallBilletter']
+    pris = ET.Element('totalPris')
+    pris.text = str(int(form['antallBilletter']) * 100)
+
+    ticket.append(name)
+    ticket.append(email)
+    ticket.append(antallBilletter)
+    ticket.append(pris)
+    ticks.append(ticket)
+
+    xml.write('static/tickets/tickets.xml',encoding='utf-8', xml_declaration=True)
+
+    #add the stylesheet to the XML file
+    dom = minidom.parse('static/tickets/tickets.xml')
+    pi = dom.createProcessingInstruction('xml-stylesheet', 'type="text/xsl" href="xml_style.xsl"')
+    root = dom.firstChild
+    dom.insertBefore(pi, root)
+    f = open('static/tickets/tickets.xml', "w")
+    string = dom.toxml();
+    f.write(string)
+    f.close()
+
+    return
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', 
